e0fed79405258faa16db7ec0a7c8eb2a4c018651
Tryller <tryllerot@gmail.com>
2016-04-19 11:11:30 -0300

    * Projetos mantidos por mim

---
+++ mantidos/AutoBuddyPlus/MainLogics/SummonerSpells.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using AutoBuddy;
+
+namespace AutoBuddy.MainLogics
+{
+    class SummonerSpells
+    {
+        public static Spell.Active Heal = null;
+        public static Spell.Active Barrier = null;
+        public static Spell.Active Ghost = null;
+
+        public static AIHeroClient target = null;
+        public void Init()
+        {
+            var heal = AutoWalker.myHero.Spellbook.Spells.Where(x => x.Name.Contains("heal"));
+            SpellDataInst Heal1 = heal.Any() ? heal.First() : null;
+            if (Heal1 != null)
+            {
+                Heal = new Spell.Active(Heal1.Slot);
+            }
+
+            var barrier = AutoWalker.myHero.Spellbook.Spells.Where(x => x.Name.Contains("barrier"));
+            SpellDataInst Barrier1 = barrier.Any() ? barrier.First() : null;
+            if (Barrier1 != null)
+            {
+                Barrier = new Spell.Active(Barrier1.Slot);
+            }
+
+            var ghost = AutoWalker.myHero.Spellbook.Spells.Where(x => x.Name.Contains("ghost"));
+            SpellDataInst Ghost1 = ghost.Any() ? ghost.First() : null;
+            if (Heal1 != null)
+            {
+                Ghost = new Spell.Active(Ghost1.Slot);
+            }
+
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private void Game_OnUpdate(EventArgs args)
+        {
+            if (AutoWalker.myHero.CountEnemiesInRange(900) > 0)
+            {
+                if (AutoWalker.myHero.HealthPercent <= 28)
+                {
+                    if (Heal != null && Heal.IsReady())
+                    {
+                        Heal.Cast();
+                    }
+                }
+                else if (AutoWalker.myHero.HealthPercent <= 18)
+                {
+                    if (Barrier != null && Barrier.IsReady())
+                    {
+                        Barrier.Cast();
+                    }
+                }
+
+                if (target == null)
+                {
+                    target = EntityManager.Heroes.Enemies.Where(en => en.Distance(AutoWalker.myHero) < 600 + en.BoundingRadius)
+                        .OrderBy(en => en.Health)
+                        .FirstOrDefault();
+                }
+
+                if (target != null && Ghost != null && Ghost.IsReady() && AutoWalker.myHero.HealthPercent() / target.HealthPercent() > 2 &&
+                        target.Distance(AutoWalker.myHero) > AutoWalker.myHero.AttackRange + target.BoundingRadius + 150 &&
+                        target.Distance(target.Position.GetNearestTurret()) > 1500)
+                {
+                    Ghost.Cast();
+                }
+            }
+        }
+    }
+}
---
+++ mantidos/AutoBuddyPlus/MyChampLogic/Sivir.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using AutoBuddy.MainLogics;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+
+namespace AutoBuddy.MyChampLogic
+{
+    internal class Sivir : IChampLogic
+    {
+        public float MaxDistanceForAA { get { return int.MaxValue; } }
+        public float OptimalMaxComboDistance { get { return AutoWalker.myHero.AttackRange; } }
+        public float HarassDistance { get { return AutoWalker.myHero.AttackRange; } }
+
+
+        public static Spell.Skillshot Q { get; private set; }
+        public static Spell.Skillshot QLine { get; private set; }
+        public static Spell.Active W { get; private set; }
+        public static Spell.Active E { get; private set; }
+        public static Spell.Active R { get; private set; }
+
+        public Sivir()
+        {
+            skillSequence = new[] { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+            ShopSequence =
+                "3340:Buy,1036:Buy,2003:StartHpPot,1053:Buy,1042:Buy,1001:Buy,3006:Buy,1036:Buy,1038:Buy,3072:Buy,2003:StopHpPot,1042:Buy,1051:Buy,3086:Buy,1042:Buy,1042:Buy,1043:Buy,3085:Buy,2015:Buy,3086:Buy,3094:Buy,1018:Buy,1038:Buy,3031:Buy,1037:Buy,3035:Buy,3033:Buy";
+
+            Q = new Spell.Skillshot(SpellSlot.Q, 1250, SkillShotType.Linear, 250, 1350, 90)
+            {
+                AllowedCollisionCount = int.MaxValue
+            };
+            QLine = new Spell.Skillshot(SpellSlot.Q, 1250, SkillShotType.Linear, 250, 1350, 90);
+            W = new Spell.Active(SpellSlot.W, 750);
+            E = new Spell.Active(SpellSlot.E);
+            R = new Spell.Active(SpellSlot.R, 1000);
+
+            Game.OnTick += Game_OnTick;
+        }
+
+        public int[] skillSequence { get; private set; }
+        public LogicSelector Logic { get; set; }
+
+        public string ShopSequence { get; private set; }
+
+        public void Harass(AIHeroClient target)
+        {
+
+        }
+
+        public void Survi()
+        {
+            AIHeroClient target =
+                EntityManager.Heroes.Enemies.FirstOrDefault(
+                    chase => chase.Distance(AutoWalker.myHero) < 600 && chase.IsVisible());
+            if (Q.IsReady())
+            {
+                if (target != null && target.IsValidTarget())
+                {
+                    Q.Cast(target.ServerPosition);
+                }
+            }
+        }
+
+        public void Combo(AIHeroClient target)
+        {
+            if (target.HealthPercent <= 45)
+            {
+                if (Q.IsReady())
+                {
+                    if (target != null && target.IsValidTarget())
+                    {
+                        Q.Cast(target.ServerPosition);
+                    }
+                }
+
+                if (W.IsReady())
+                {
+                    if (target != null && target.IsValidTarget())
+                    {
+                        W.Cast();
+                        Orbwalker.ResetAutoAttack();
+                    }
+                }
+
+                if (R.IsReady())
+                {
+                    if (ObjectManager.Player.Position.CountAlliesInRange(1000) >= 2
+                      && ObjectManager.Player.Position.CountEnemiesInRange(2000) >= 2)
+                    {
+                        R.Cast();
+                    }
+                }
+            }
+        }
+
+        private void Game_OnTick(System.EventArgs args)
+        {
+
+        }
+    }
+}
