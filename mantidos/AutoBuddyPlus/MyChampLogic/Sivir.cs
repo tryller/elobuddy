cd8211c1eb718cd4e5f4527526d2ba836e69d70a
Tryller <tryllerot@gmail.com>
2016-04-19 11:22:09 -0300

    * Update sivir para usar o E

--- mantidos/AutoBuddyPlus/MyChampLogic/Sivir.cs
+++ mantidos/AutoBuddyPlus/MyChampLogic/Sivir.cs
@@ -35,12 +35,133 @@ namespace AutoBuddy.MyChampLogic
             R = new Spell.Active(SpellSlot.R, 1000);
 
             Game.OnTick += Game_OnTick;
+            Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast; 
         }
 
         public int[] skillSequence { get; private set; }
         public LogicSelector Logic { get; set; }
 
         public string ShopSequence { get; private set; }
+
+        private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            var unit = sender as AIHeroClient;
+            if (unit == null || !unit.IsValid)
+            {
+                return;
+            }
+
+            if (!unit.IsEnemy || !E.IsReady())
+            {
+                return;
+            }
+
+            if (args.End.Distance(Player.Instance) == 0)
+                return;
+
+            var type = args.SData.TargettingType;
+
+            if (unit.ChampionName.Equals("Caitlyn") && args.Slot == SpellSlot.Q)
+            {
+                Core.DelayAction(() => E.Cast(),
+                    (int)(args.Start.Distance(Player.Instance) / args.SData.MissileSpeed * 1000) -
+                    (int)(args.End.Distance(Player.Instance) / args.SData.MissileSpeed) - 500);
+            }
+            if (unit.ChampionName.Equals("Zyra"))
+            {
+                Core.DelayAction(() => E.Cast(),
+                    (int)(args.Start.Distance(Player.Instance) / args.SData.MissileSpeed * 1000) -
+                    (int)(args.End.Distance(Player.Instance) / args.SData.MissileSpeed) - 500);
+            }
+            if (args.End.Distance(Player.Instance) < 250)
+            {
+                if (unit.ChampionName.Equals("Bard") && args.End.Distance(Player.Instance) < 300)
+                {
+                    Core.DelayAction(() => E.Cast(), (int)(unit.Distance(Player.Instance) / 7f) + 400);
+                }
+                else if (unit.ChampionName.Equals("Ashe"))
+                {
+                    Core.DelayAction(() => E.Cast(),
+                        (int)(args.Start.Distance(Player.Instance) / args.SData.MissileSpeed * 1000) -
+                        (int)args.End.Distance(Player.Instance));
+                    return;
+                }
+                else if (unit.ChampionName.Equals("Varus") || unit.ChampionName.Equals("TahmKench") ||
+                         unit.ChampionName.Equals("Lux"))
+                {
+                    Core.DelayAction(() => E.Cast(),
+                        (int)(args.Start.Distance(Player.Instance) / args.SData.MissileSpeed * 1000) -
+                        (int)(args.End.Distance(Player.Instance) / args.SData.MissileSpeed) - 500);
+                }
+                else if (unit.ChampionName.Equals("Amumu"))
+                {
+                    if (sender.Distance(Player.Instance) < 1100)
+                        Core.DelayAction(() => E.Cast(),
+                            (int)(args.Start.Distance(Player.Instance) / args.SData.MissileSpeed * 1000) -
+                            (int)(args.End.Distance(Player.Instance) / args.SData.MissileSpeed) - 500);
+                }
+            }
+
+            if (args.Target != null && type.Equals(SpellDataTargetType.Unit))
+            {
+                if (!args.Target.IsMe ||
+                    (args.Target.Name.Equals("Barrel") && args.Target.Distance(Player.Instance) > 200 &&
+                     args.Target.Distance(Player.Instance) < 400))
+                {
+                    return;
+                }
+
+                if (unit.ChampionName.Equals("Nautilus") ||
+                    (unit.ChampionName.Equals("Caitlyn") && args.Slot.Equals(SpellSlot.R)))
+                {
+                    var d = unit.Distance(Player.Instance);
+                    var travelTime = d / args.SData.MissileSpeed;
+                    var delay = travelTime * 1000 - E.CastDelay + 1150;
+                    //Console.WriteLine("TT: " + travelTime + " " + delay);
+                    Core.DelayAction(() => E.Cast(), (int)delay);
+                    return;
+                }
+                E.Cast();
+            }
+
+            if (type.Equals(SpellDataTargetType.Unit))
+            {
+                if (unit.ChampionName.Equals("Bard") && args.End.Distance(Player.Instance) < 300)
+                {
+                    Core.DelayAction(() => E.Cast(), 400 + (int)(unit.Distance(Player.Instance) / 7f));
+                }
+                else if (unit.ChampionName.Equals("Riven") && args.End.Distance(Player.Instance) < 260)
+                {
+                    E.Cast();
+                }
+                else
+                {
+                    E.Cast();
+                }
+            }
+            else if (type.Equals(SpellDataTargetType.LocationAoe) &&
+                     args.End.Distance(Player.Instance) < args.SData.CastRadius)
+            {
+                // annie moving tibbers
+                if (unit.ChampionName.Equals("Annie") && args.Slot.Equals(SpellSlot.R))
+                {
+                    return;
+                }
+                E.Cast();
+            }
+            else if (type.Equals(SpellDataTargetType.Cone) &&
+                     args.End.Distance(Player.Instance) < args.SData.CastRadius)
+            {
+                E.Cast();
+            }
+            else if (type.Equals(SpellDataTargetType.SelfAoe) || type.Equals(SpellDataTargetType.Self))
+            {
+                var d = args.End.Distance(Player.Instance.ServerPosition);
+                var p = args.SData.CastRadius > 5000 ? args.SData.CastRange : args.SData.CastRadius;
+                if (d < p)
+                    E.Cast();
+            }
+        }
 
         public void Harass(AIHeroClient target)
         {
